9fd3c643252a1c464b67d0a5ad212aa3d4f85a68
Your Name <marielmetalios@gmail.com>
2025-01-01 10:02:06 -0500

    pushing latest notes

--- server.js
+++ server.js
@@ -1,24 +1,29 @@
 import http from 'http';
-const PORT = 8000;
+const PORT = process.env.PORT;
 
 // you can also set header types below
 const server = http.createServer((req, res) => {
-    res.write('Hello World!');
-    res.end();
-})
-// above we are sending text to the browser
+    console.log(req.url);
+    console.log(req.method);
+    // res.write('Hello World!');
+    // res.end();
+// })
+// // above we are sending text to the browser
 // if we were using Express, it would end this automatically
+
+// combine methods:
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end('<h1>Hello World</h1>');
+});
 
 // call listen method, usually use 5000 and then we pass in a function after it connects:
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+    console.log(`Server running on port ${PORT}`);
+});
 
 // you can also run status errors
-res.statusCode = 404
+// res.statusCode = 404
 
-combine methods :
-
-res.writeHead(500, { 'Content-Type = 'application/json' })
-res.end
+// .env file
+// environment variables
 
