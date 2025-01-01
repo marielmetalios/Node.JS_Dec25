8d929b80b23b5b622915eaed734f4a1c5fb2d92e
Your Name <marielmetalios@gmail.com>
2025-01-01 11:30:39 -0500

    updating server.js with error messages

--- server.js
+++ server.js
@@ -3,18 +3,34 @@ const PORT = process.env.PORT;
 
 // you can also set header types below
 const server = http.createServer((req, res) => {
-    console.log(req.url);
-    console.log(req.method);
+    try {
+        if(req.method === 'GET') {
+
+    } else {
+        throw new Error('Method not allowed')
+    } catch (error)
+    }
+
+    // console.log(req.url);
+    // console.log(req.method);
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>Homepage</h1>');
+    } else if (req.url === '/about') {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end('<h1>About</h1>'); 
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/html' });
+        res.end('<h1>Not Found</h1>');
+    }
+});
     // res.write('Hello World!');
     // res.end();
 // })
 // // above we are sending text to the browser
 // if we were using Express, it would end this automatically
 
-// combine methods:
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('<h1>Hello World</h1>');
-});
 
 // call listen method, usually use 5000 and then we pass in a function after it connects:
 server.listen(PORT, () => {
@@ -26,4 +42,5 @@ server.listen(PORT, () => {
 
 // .env file
 // environment variables
+// postman plug ins will allow http post request
 
